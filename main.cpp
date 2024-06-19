3e7de104a7fe562c017767790446578f3456110a
Elan-gab <gabrielelansoares@gmail.com>
2024-06-18 21:07:56 -0300

    criacao main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
-#include "Core/include/Funcionario.hpp"
-#include "Core/include/Setor.hpp"
+#include "AreaTrab/include/AreaFront.hpp"
+#include "AreaTrab/include/Areaback.hpp"
+#include "AreaTrab/include/AreaEngenharia.hpp"
+
+#include "Funcionarios/include/DesenvBack.hpp"
+#include "Funcionarios/include/DesenvFront.hpp"
+#include "Funcionarios/include/DesenvFull.hpp"
+#include "Funcionarios/include/EngenheiroS.hpp"
 #include "Funcionarios/include/Estagiario.hpp"
+
 
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
 
+    vector<Setor*> setores;
 
+    setores.push_back(new AreaEngenharia());
+    setores.push_back(new AreaFront());
+    setores.push_back(new AreaBack());
+
+    //setores[0].
 
 }
